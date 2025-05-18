8ae6e17255fb9e86ab35efc3558c8594d031d109
OfirSchor <ofirschor@mail.tau.ac.il>
2025-05-18 17:39:43 +0300

    Update contact_order_calculation.py

--- calculate contact order/contact_order_calculation.py
+++ calculate contact order/contact_order_calculation.py
@@ -5,16 +5,16 @@ csu_data = pd.read_csv("csu_df_for_example.csv", index_col=0)
 def calc_contact_order(csu_df, min_seq_distance = 4, prefix=""):
     df = csu_df[csu_df["residual_dist"] >= min_seq_distance]
 
-    amino_dist_precentile = (df.groupby(["target_protein"])
+    amino_dist = (df.groupby(["target_protein"])
         .agg(sum_residual_dist=("residual_dist","sum"),
              count_residual_dist=("residual_dist","count"))
         .reset_index()
         )
-    amino_dist_precentile.columns = ["target_protein", "sum_residual_dist", "count_residual_dist"]
-    amino_dist_precentile[prefix + "abs_mean_contact_order"] = amino_dist_precentile["sum_residual_dist"] / amino_dist_precentile["count_residual_dist"]
-    amino_dist_precentile.drop(["sum_residual_dist", "count_residual_dist"], axis=1)
+    amino_dist.columns = ["target_protein", "sum_residual_dist", "count_residual_dist"]
+    amino_dist[prefix + "abs_mean_contact_order"] = amino_dist["sum_residual_dist"] / amino_dist["count_residual_dist"]
+    amino_dist.drop(["sum_residual_dist", "count_residual_dist"], axis=1)
 
-    return amino_dist_precentile
+    return amino_dist
 
 display(calc_contact_order(protein_data_csu))
 
